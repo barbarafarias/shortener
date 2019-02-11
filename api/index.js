96518cc9e81412dab7d7844f31af34752ebd9398
barbarafarias <barbara.fs@gmail.com>
2019-02-11 15:08:27 -0200

    validation fix

--- api/index.js
+++ api/index.js
@@ -9,19 +9,14 @@ const getShortenedUrl = () => {
 const short = async (originalUrl) => {
     let data, res
     try {
-        res = await db.getUrlByOriginalUrl(originalUrl);
-        if (res.rowCount > 0) {
-            data = res.rows[0];
-        } else {
-            let shortenedUrl = getShortenedUrl();
+        let shortenedUrl = getShortenedUrl();
+        res = await db.getUrlByShortenedUrl(shortenedUrl);
+        while (res.rowCount > 0) {
+            shortenedUrl = getShortenedUrl();
             res = await db.getUrlByShortenedUrl(shortenedUrl);
-            while (res.rowCount > 0) {
-                shortenedUrl = getShortenedUrl();
-                res = await db.getUrlByShortenedUrl(shortenedUrl);
-            }
-            res = await db.insertUrl(originalUrl, shortenedUrl);
-            data = res.rows[0];    
         }
+        res = await db.insertUrl(originalUrl, shortenedUrl);
+        data = res.rows[0];    
         
     } catch(err) {
         console.log(err)
